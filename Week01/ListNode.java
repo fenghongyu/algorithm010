f30d20081a90394b25a88e8d4f654d1c1690c1c3
fenghongyu <fenghongyu@shuidihuzhu.com>
2020-06-14 20:53:47 +0800

    MergeTwoLists

---
+++ Week01/ListNode.java
@@ -0,0 +1,13 @@
+/**
+ * @CLASSNAME:
+ * @AUTHOR: fenghongyu
+ * @TIME: 2020/6/14 20:52
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
